4feb02674f2df141f59c91bb5e0041a4c72fa09b
Fernando De Leon <gasnare@hotmail.com>
2021-05-02 20:26:25 -0500

    Delete debugger mode banner

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,10 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Pizza Order', home: PizzaOrderDetails());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Pizza Order',
+      home: PizzaOrderDetails(),
+    );
   }
 }
