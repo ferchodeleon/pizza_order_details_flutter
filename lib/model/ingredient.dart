4d3985fb0906b9cb4d697a7b7632398f0b21b1bb
Fernando De Leon <gasnare@hotmail.com>
2021-05-02 20:24:31 -0500

    Data to image for list ingredients

---
+++ lib/model/ingredient.dart
@@ -0,0 +1,13 @@
+class Ingredient {
+  const Ingredient(this.image);
+  final String image;
+}
+
+final ingredients = const <Ingredient>[
+  Ingredient('assets/chili.png'),
+  Ingredient('assets/olive.png'),
+  Ingredient('assets/onion.png'),
+  Ingredient('assets/pea.png'),
+  Ingredient('assets/pickle.png'),
+  Ingredient('assets/potato.png'),
+];
