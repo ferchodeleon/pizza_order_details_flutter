ca4839e29df4e23783740b0949acf021e8418a2b
Fernando De Leon <gasnare@hotmail.com>
2021-06-06 18:12:24 -0500

    Add to new ingredients

--- lib/model/ingredient.dart
+++ lib/model/ingredient.dart
@@ -1,15 +1,72 @@
+import 'package:flutter/cupertino.dart';
+
 class Ingredient {
-  const Ingredient(this.image);
+  const Ingredient(this.image, this.positions);
   final String image;
+  final List<Offset> positions;
 
   bool compare(Ingredient ingredient) => ingredient.image == image;
 }
 
 final ingredients = const <Ingredient>[
-  Ingredient('assets/chili.png'),
-  Ingredient('assets/olive.png'),
-  Ingredient('assets/onion.png'),
-  Ingredient('assets/pea.png'),
-  Ingredient('assets/pickle.png'),
-  Ingredient('assets/potato.png'),
+  Ingredient(
+    'assets/chili.png',
+    <Offset>[
+      Offset(0.2, 0.2),
+      Offset(0.6, 0.2),
+      Offset(0.4, 0.25),
+      Offset(0.5, 0.3),
+      Offset(0.4, 0.65),
+    ],
+  ),
+  Ingredient(
+    'assets/olive.png',
+    <Offset>[
+      Offset(0.2, 0.35),
+      Offset(0.65, 0.35),
+      Offset(0.3, 0.23),
+      Offset(0.5, 0.2),
+      Offset(0.3, 0.5),
+    ],
+  ),
+  Ingredient(
+    'assets/onion.png',
+    <Offset>[
+      Offset(0.2, 0.65),
+      Offset(0.65, 0.3),
+      Offset(0.25, 0.25),
+      Offset(0.45, 0.35),
+      Offset(0.4, 0.65),
+    ],
+  ),
+  Ingredient(
+    'assets/pea.png',
+    <Offset>[
+      Offset(0.2, 0.35),
+      Offset(0.65, 0.35),
+      Offset(0.3, 0.23),
+      Offset(0.5, 0.2),
+      Offset(0.3, 0.5),
+    ],
+  ),
+  Ingredient(
+    'assets/pickle.png',
+    <Offset>[
+      Offset(0.2, 0.65),
+      Offset(0.65, 0.3),
+      Offset(0.25, 0.25),
+      Offset(0.45, 0.35),
+      Offset(0.4, 0.65),
+    ],
+  ),
+  Ingredient(
+    'assets/potato.png',
+    <Offset>[
+      Offset(0.2, 0.2),
+      Offset(0.6, 0.2),
+      Offset(0.4, 0.25),
+      Offset(0.5, 0.3),
+      Offset(0.4, 0.65),
+    ],
+  ),
 ];
