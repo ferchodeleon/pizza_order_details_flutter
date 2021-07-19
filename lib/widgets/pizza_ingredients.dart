369ec79a7ee1eab40f2b390e90a1041f7f4e2c20
Fernando De León Gil <fernandodeleongil@MacBook-Air-de-Fernando.local>
2021-07-18 22:00:57 -0500

    add to deleted ingredient and modify disabled ingredient

--- lib/widgets/pizza_ingredients.dart
+++ lib/widgets/pizza_ingredients.dart
@@ -19,6 +19,7 @@ class PizzaIngredients extends StatelessWidget {
             return PizzaIngredientsItem(
               ingredient: ingredient,
               exits: bloc.containsIngredient(ingredient),
+              onTap: () => bloc.removeIngredient(ingredient),
             );
           },
         );
@@ -28,28 +29,33 @@ class PizzaIngredients extends StatelessWidget {
 }
 
 class PizzaIngredientsItem extends StatelessWidget {
-  const PizzaIngredientsItem({Key key, this.ingredient, this.exits})
-      : super(key: key);
+  const PizzaIngredientsItem({
+    Key key,
+    this.ingredient,
+    this.exits,
+    this.onTap,
+  }) : super(key: key);
 
   final Ingredient ingredient;
   final bool exits;
+  final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
-    final _buildChild = Container(
-      height: 60.0,
-      width: 60.0,
-      decoration: BoxDecoration(
-        color: Color(0xFFF5EED3),
-        shape: BoxShape.circle,
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(3.0),
+    final _buildChild = GestureDetector(
+      onTap: exits ? onTap : null,
+      child: Container(
+        height: 60.0,
+        width: 60.0,
+        decoration: BoxDecoration(
+          color: Color(0xFFF5EED3),
+          shape: BoxShape.circle,
+        ),
         child: exits
-            ? ColorFiltered(
-                colorFilter: ColorFilter.mode(
-                  Colors.black12,
-                  BlendMode.saturation,
+            ? Container(
+                decoration: BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.circular(50.0),
                 ),
                 child: Image.asset(
                   ingredient.image,
